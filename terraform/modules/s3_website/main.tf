a828ce7cbd0ddc20d125d70e520e0b5912f4d5a7
Baris Sayil <barissayil@protonmail.com>
2023-08-22 10:46:25 +0200

    🧱 Remove unneeded arguments

--- terraform/modules/s3_website/main.tf
+++ terraform/modules/s3_website/main.tf
@@ -10,11 +10,6 @@ resource "aws_s3_bucket" "website" {
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
